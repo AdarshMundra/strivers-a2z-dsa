f0bbef2e2cc697750043daf510c64d3575b4e4e0
Adarshmundra <adarsh.mundra1305@gmail.com>
2023-12-24 12:25:08 +0530

    first commit

---
+++ Learn the basics/Things to Know in Python/User_Input_Output.py
@@ -0,0 +1,19 @@
+x=input()
+
+if x.isalpha():
+
+    if x.isupper():
+
+        print("1")
+
+    elif x.islower():
+
+        print("0")
+
+    else:
+
+        print("-1")
+
+else:
+
+    print("-1")
